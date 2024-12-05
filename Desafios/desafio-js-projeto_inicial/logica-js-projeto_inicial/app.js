dfb9563f68717d82e0bb1366a2ce954c2f01195c
RainerVS1 <Vitorgaming871@gmail.com>
2024-12-05 16:01:58 -0300

    create: initial JavaScript projects

---
+++ Desafios/desafio-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -0,0 +1,37 @@
+// Dia da Semana
+diaDaSemana = prompt("Qual dia da semana?")
+if (diaDaSemana == `Sábado`) {
+    alert("Bom fim de semana!")
+}
+else if (diaDaSemana == "Domingo") {
+    alert("Bom fim de samana!")
+}
+else {
+    alert("Boa semana!")
+}
+
+// Verificador de número
+numeroVerificar = prompt("Digite o número")
+if ( numeroVerificar > 0) {
+    alert("Número Positivo!")
+}
+else {
+    alert ("Número Negativo!")
+}
+
+// Sistema de pontuação
+pontuacao = 90
+if (pontuacao >= 100) { 
+   console.log("Parabéns, você venceu!")
+}
+else {
+   console.log("Tente novamente para ganhar")
+}
+
+// Saldo da conta
+let saldoDaconta = 59.12
+alert (`Seu saldo é de ${saldoDaconta}.`)
+
+//nome
+seuNome = prompt("Qual seu nome?")
+alert(`Bem vindo: ${seuNome}`)
---
+++ Rainer/logica-js-projeto_inicial/app.js
@@ -0,0 +1,11 @@
+alert("Bem vindo ao Liberalismo");
+let numeroSecreto = 16;
+console.log (numeroSecreto)
+let chute = prompt("Escolha um número entre 1 e 31");
+
+if (numeroSecreto == chute ) {
+    alert(`Parabéns! Você descobriu o aniversário de John Locke dia ${numeroSecreto}`);
+}
+else {
+    alert(" Você errou! tente pesquisar a data de aniversário do Pai do Liberalismo")
+}
