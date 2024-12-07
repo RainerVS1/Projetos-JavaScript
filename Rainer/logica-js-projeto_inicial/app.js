cc31d6d2357347860143e7e2032fa8feccf52d9a
RainerVS1 <Vitorgaming871@gmail.com>
2024-12-07 10:50:05 -0300

    update

--- Rainer/logica-js-projeto_inicial/app.js
+++ Rainer/logica-js-projeto_inicial/app.js
@@ -1,11 +1,25 @@
-alert("Bem vindo ao Liberalismo");
-let numeroSecreto = 16;
-console.log (numeroSecreto)
-let chute = prompt("Escolha um número entre 1 e 31");
+alert("Bem vindo ao contador de livros liberais!");
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
+console.log (numeroSecreto);
+let chute = prompt("Escolha um número entre 1 e 100");
+let tentativas = 1
 
-if (numeroSecreto == chute ) {
-    alert(`Parabéns! Você descobriu o aniversário de John Locke dia ${numeroSecreto}`);
+// Enquanto chute não foir igual ao N.S
+while (chute != numeroSecreto) {
+    chute = prompt("Escolha um número entre 1 e 100");
+    // se o chute for igual ao número secreto
+    if (numeroSecreto == chute ) {
+        break; 
+    } else {
+        if (chute > numeroSecreto) {
+            alert (`${chute}? CALMA SEU LEITOR! comece com um pouco menos`);
+        } else {
+            alert(`${chute} é Muito pouco, seja mais ambicioso! `);
+        }
+    //tentativas
+    tentativas++;   
+    } 
 }
-else {
-    alert(" Você errou! tente pesquisar a data de aniversário do Pai do Liberalismo")
-}
+
+let palavratentativas = tentativas > 1 ? `tentativas` : `tentativa`;
+alert(`Parabéns! Você descobriu a quantidade de livros liberis que terá que lê, ${numeroSecreto} com ${tentativas} ${palavratentativas}.`)
